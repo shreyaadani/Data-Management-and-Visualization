fcff9f9e2ed062fb55be700fae4b3616e4b1b8b5
shreyaadani <59827804+shreyaadani@users.noreply.github.com>
2020-07-24 01:20:56 +0530

    Add files via upload

---
+++ assignment3.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 22:39:14 2020
+
+@author: Shreyaa Dani
+"""
+
+
+#On the topic I have selected, the program would be:
+
+
+import pandas as pd
+import numpy as np
+
+
+data = pd.read_csv('f:\\nesarc_pds.csv', low_memory=False)
+
+sub = data[data['S1Q213'] <= 1]
+# a copy of dataset where undesired data has been removed
+
+c4 = data['S1Q213'].value_counts(dropna = False)
+d4 =  sub['S1Q213'].value_counts(dropna = False)
+print(f"DURING PAST 4 WEEKS, HOW OFTEN FELT DOWNHEARTED AND DEPRESSED:")
+print('BEFORE:')
+print(c4)
+print('AFTER')
+print(d4)
+
+print("-"*42)
+
+d3=data['S4AQ4A18'].value_counts(dropna = False)
+c1 = sub['S4AQ4A18'].value_counts(dropna = False)
+print(f"\WANTED TO DIE, YES=1:" )
+print('BEFORE:')
+print(d3)
+print('AFTER')
+print(c1)
+
+
+
+
+d3=data['S4AQ11'].value_counts(dropna = False)
+c1 = sub['S4AQ11'].value_counts(dropna = False)
+#sub['S4AQ11'] = sub['S4AQ11' ].replace(i, np.nan) #refused, dont know and not applicable converted to NaN
+print(f"\ANY EPISODE BEGAN AFTER DRINKING HEAVILY/MORE THAN USUAL" )
+print('BEFORE:')
+print(d3)
+print('AFTER')
+print(c1)
+
+
+
+
+d3=data['S7Q31B '].value_counts(dropna = False)
+c1 = sub['S7Q31B '].value_counts(dropna = False)
+#sub['S7Q31B '] = sub['S7Q31B ].replace(i, np.nan) #refused, dont know and not applicable converted to NaN
+print(f"\DRANK ALCOHOL TO AVOID SOCIAL PHOBIA IN LAST 12 MONTHS YES=1" )
+print('BEFORE:')
+print(d3)
+print('AFTER')
+print(c1)
+
+
+
+
